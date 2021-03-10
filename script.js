8373500ae057ded261e1259fe2fdeb766a749e47
JFNCGamer <78734362+JFNCGamer@users.noreply.github.com>
2021-03-10 12:29:39 +0000

    Update

--- script.js
+++
@@ -1,6 +0,0 @@
-function Function1() {
- document.getElementById("creditos").innerHTML = "Créditos: João: programador. Joana: Editora de texto";
-}
-function Function2() {
- document.getElementById("creditos").innerHTML = "";
-}
